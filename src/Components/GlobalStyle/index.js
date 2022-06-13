084eb62dc77f6de6d0a7edc8da580d86a65c3cde
Hoang Quyen <hoangquyen18012001@gmail.com>
2022-06-13 15:14:23 +0700

    Chnage

--- src/Components/GlobalStyle/index.js
+++ src/Components/GlobalStyle/index.js
@@ -1,10 +1,1 @@
-import './GlobalStyle.scss';
-import React from 'react';
-import PropTypes from 'prop-types';
-function GlobalStyle({ children }) {
-    return children;
-}
-GlobalStyle.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-export default GlobalStyle;
+export { default } from './GlobalStyle';
